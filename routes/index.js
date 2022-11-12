c859aad0f7672a2a14c23ddcff57e9b2212e4b08
unknown <tafedericoemanuel@gmail.com>
2022-11-12 01:28:06 -0300

    mt-01-18 alpha2

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 let router = require("express").Router;
 
 let user = require("./api/users")
+let city = require('./city');
 
 
 router.use("/api/users", user)
+router.use('/api/cities', city);
+
 
 module.exports = router;
