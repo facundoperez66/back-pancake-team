af0e63c6a0c866d62ff0f5c3eb110b4b72214b34
unknown <tafedericoemanuel@gmail.com>
2022-11-13 13:23:18 -0300

    mt-b1-31

--- routes/itinerary.js
+++ routes/itinerary.js
@@ -1,7 +1,8 @@
 let router = require('express').Router();
 
-let {read} = require('../controllers/itinerary');
+let {read, create } = require('../controllers/itinerary');
 
 router.get('/', read);
+router.post('/', create)
 
 module.exports = router;
