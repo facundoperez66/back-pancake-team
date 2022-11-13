a63fdbdc38568ab9f2c453c9f989af52ee6c9345
unknown <tafedericoemanuel@gmail.com>
2022-11-13 12:22:31 -0300

    mt-01-19

--- controllers/city.js
+++ controllers/city.js
@@ -15,7 +15,43 @@ const controller = {
                 message: error.message,
             });
         }
-    }
+    },
+    read: async (req, res) => {
+
+        let query = {};
+        if (req.query.continent) {
+            query = {
+                continent: req.query.continent
+            };
+        }
+        if (req.query.name) {
+            query = {
+                ...query,
+                name: { $regex: req.query.name, $options: 'i' },
+            };
+        }
+
+        try {
+            let allCities = await City.find(query);
+            if (allCities) {
+                res.status(200).json({
+                    success: true,
+                    message: 'All cities',
+                    data: allCities,
+                })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: 'No cities found',
+                });
+            }
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    },
 }
 
 module.exports = controller
--- routes/city.js
+++ routes/city.js
@@ -1,7 +1,8 @@
 let router = require('express').Router();
 
-let { create } = require('../controllers/city');
+let { create, read } = require('../controllers/city');
 
 router.post('/', create);
+router.get('/', read)
 
 module.exports = router;
