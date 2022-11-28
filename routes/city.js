be100f7b3b668aa6bbd34e0c9dfa56b2c8310c64
FedericoRodriguez7 <tafedericoemanuel@gmail.com>
2022-11-28 08:10:20 -0300

    MT-01-53

--- routes/city.js
+++ routes/city.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const validator = require("../middlewares/validator")
 const schema = require("../schemas/newCity")
+const passport = require("passport")
 
 
 let { create, read, readOne, update, destroyOne, readAdminCities } = require('../controllers/city');
@@ -13,8 +14,8 @@ router.route("/")
 
 router.route('/:id')
     .get(readOne)
-    .put(update)
-    .delete(destroyOne)
+    .put(passport.authenticate("jwt", { session: false }),update)
+    .delete(passport.authenticate("jwt", { session: false }),destroyOne)
 
 
 module.exports = router;
--- test/city-test.js
+++ test/city-test.js
@@ -18,35 +18,24 @@ describe('Cities', function (done) {
                 done();
             });
     })
-    describe('POST /hotels', function () {
-
-        it('capacity should be a number', function (done) {
-            request(app)
-                .post('/api/hotels')
-                .send({
-                    name: "hotel Rio de Janeiro",
-                    photo: "https://www.eltiempo.com/files/image_640_428/files/crop/uploads/2022/05/06/62752986a0b9f.r_1651848957740.0-73-1500-823.jpeg",
-                    capacity:5020,
-                    cityID:"636d3af27ccd7c6ea97b82e4",
-                    userID:"636d210297606439046194bb"
-                })
-                .expect(res=>{
-                    let response = res.body.response.capacity
-                    console.log(response)
-                    assert.isNumber(response,"is number")
-                })
-                .expect(res=>{
-                    let response = res.status
-                    assert.strictEqual(response,201)
-                })
-                .end((err, res) => {
-                    if(err){
-                        return done(err)
-                    }
-                    return done()
-                })
-        })})
-    
+    it('verify that the user sends a string in the name field when creating a city', function (done) {
+        request(app)
+            .post(`/api/cities/`)
+            .send({
+                name: "Paris",
+                continent: "Europe",
+                photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/1200px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg",
+                population: 2229621,
+                userId: "636fe5cd55d86e11bfaebc4a"
+            })
+            .expect(res => {
+                assert.isString(res.body.new_city.name)
+            })
+            .end(function (err, res) {
+                if (err) return done(err);
+                done();
+            });
+    })
     it('Verify that returns success false when unable to create a city', function (done) {
         request(app)
             .post(`/api/cities/`)
@@ -59,5 +48,20 @@ describe('Cities', function (done) {
                 done();
             });
     })
+
+    it("Delete a city successfully", function (done) {
+        token = 'ya29.a0AeTM1icu-VfS6iuqLHuqi8G-n1sadVgygBq54QEeKnz5uF5tUvkUvJO9YuK7GoA_192oZCpovEF-2M0tZAJTDhgLPZs7t_ZDlKvQKOSlaGttDP76dhr9S2ZpAb1ADVdEWK_13o76-MY9pC0Uyq7ecKmwN67BaCgYKAaYSARMSFQHWtWOmXWEJWHfKlCP8D25SWi2zJA0163'
+        idCity = '637084d4caa81e856ac3012f'
+        request(app)
+            .delete(`/api/cities/${idCity}`)
+            .auth(token, { type: "bearer" })
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                done();
+            });
+    });
 
 })
